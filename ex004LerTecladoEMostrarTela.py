d8f833e88cb955031672f667bd38e661cb4a9a37
crizostomo <diogo_crizostomo@hotmail.com>
2021-11-05 21:19:27 -0300

    ex004 has been added

---
+++ ex004LerTecladoEMostrarTela.py
@@ -0,0 +1,6 @@
+a = input('type something: ')
+print('the primitive type of this value is: ', type(a))
+print('There are only spaces?', a.isspace())
+print('Is it a number?', a.isnumeric())
+print('Is it alphabetic?', a.isalpha())
+print('Is it alphanumeric?'), a.isalnum()
