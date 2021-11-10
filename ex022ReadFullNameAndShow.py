08204b1cb3f6f1033abe0c5430879dd052a7da73
crizostomo <diogo_crizostomo@hotmail.com>
2021-11-10 08:59:01 -0300

    ex022 has been added

---
+++ ex022ReadFullNameAndShow.py
@@ -0,0 +1,6 @@
+phrase = 'Pedro Alvarez Cabral'
+print('upper = ', phrase.upper())
+print('lower = ', phrase.lower())
+AmountOfLetters = phrase.split()
+print('First name contains: ', len(AmountOfLetters[0]))
+print(len(AmountOfLetters))
