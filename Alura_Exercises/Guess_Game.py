d2c69e60759fd7a2b807b246c9e44decfadc801e
crizostomo <diogo_crizostomo@hotmail.com>
2021-11-30 18:45:07 -0300

    exercise updated

--- Alura_Exercises/Guess_Game.py
+++ Alura_Exercises/Guess_Game.py
@@ -3,22 +3,29 @@ print('Welcome to the guess game')
 print('*'*20)
 
 secret_number = 42
+guess_total = 3
+# -->Usar com While round = 1
 
-guess_str = input('Please type your number: ')
+# -->Usar com While while(round <= guess_total):
+for round in range(1, guess_total + 1):
+    print('Guess {} of {}'.format(round, guess_total))
+    guess_str = input('Please type your number: ')
+    print('You typed {}'.format(guess_str))
+    guess   = int(guess_str)
 
-print('You typed {}'.format(guess_str))
+    succed  = secret_number == guess_str
+    higher  = guess > secret_number
+    lower   = guess < secret_number
 
-guess   = int(guess_str)
-succed  = secret_number == guess_str
-higher  = guess > secret_number
-lower   = guess < secret_number
+    if(succed):
+        print('You guessed right')
+    else:
+        if(higher):
+            print('You failed! Your guess was higher than the secret number.')
+        elif(lower):
+            print('You failed! Your guess was lower than the secret number.')
 
-if(succed):
-    print('You guessed right')
-else:
-    if(higher):
-        print('You failed! Your guess was higher than the secret number.')
-    elif(lower):
-        print('You failed! Your guess was lower than the secret number.')
+#    guess_total = guess_total - 1
+# -->Usar com While    round = round + 1
 
 print("Game over")
