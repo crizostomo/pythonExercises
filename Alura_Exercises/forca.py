2970ada306076fe21a3001d37d2aa87f90cedbb4
crizostomo <diogo_crizostomo@hotmail.com>
2021-12-06 08:13:14 -0300

    forca game has been finished

--- Alura_Exercises/forca.py
+++ Alura_Exercises/forca.py
@@ -1,24 +1,20 @@
 import random
 
 
-
 def play():
 
     print_opening_message()
     secret_word = loading_secret_word()
-
 
 #    print(words)
 
 #    secret_word = "java".upper()
 #    letters_right = ["_" for letter in secret_word]
     letters_right = initiate_letters_right(secret_word)
-
+    print(letters_right)
 
 #    for letter in letters_right:
 #       letters_right.append("_")
-
-    print(letters_right)
 
     hung = False
     got_it_right = False
@@ -27,31 +23,116 @@ def play():
 
     while(not hung and not got_it_right):
 
-        guess = input('What letter? ')
-        guess = guess.strip().upper()
+        guess = ask_guess()
 
         if(guess in secret_word):
-            index = 0
-            for letter in secret_word:
-                if(guess == letter):
-                    letters_right[index] = letter
-    #                print('Found the letter {} in the position {}'.format(letter, index))
-                index += + 1
+            check_correct_guess(guess, letters_right, secret_word)
         else:
             errors += + 1
+            hung_draw(errors)
 
-        hung = errors == 6
+        hung = errors == 7
         got_it_right = "_" not in letters_right #Acertou quando o '_' não estiver em letras acertadas
         print(letters_right)
 
     if(got_it_right):
-        print('You won!!')
+        print("Congrats, you won!")
+        print("       ___________      ")
+        print("      '._==_==_=_.'     ")
+        print("      .-\\:      /-.    ")
+        print("     | (|:.     |) |    ")
+        print("      '-|:.     |-'     ")
+        print("        \\::.    /      ")
+        print("         '::. .'        ")
+        print("           ) (          ")
+        print("         _.' '._        ")
+        print("        '-------'       ")
     else:
-        print('You lost!!')
+        print("Well, you were hung!")
+        print("The word was {}".format(secret_word))
+        print("    _______________         ")
+        print("   /               \       ")
+        print("  /                 \      ")
+        print("//                   \/\  ")
+        print("\|   XXXX     XXXX   | /   ")
+        print(" |   XXXX     XXXX   |/     ")
+        print(" |   XXX       XXX   |      ")
+        print(" |                   |      ")
+        print(" \__      XXX      __/     ")
+        print("   |\     XXX     /|       ")
+        print("   | |           | |        ")
+        print("   | I I I I I I I |        ")
+        print("   |  I I I I I I  |        ")
+        print("   \_             _/       ")
+        print("     \_         _/         ")
+        print("       \_______/           ")
     print("Game over")
 
 
 
+def hung_draw(errors):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if (errors == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if (errors == 2):
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if (errors == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if (errors == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if (errors == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if (errors == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (errors == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
+
+
+def check_correct_guess(guess, letters_right, secret_word):
+    index = 0
+    for letter in secret_word:
+        if (guess == letter):
+            letters_right[index] = letter
+        #                print('Found the letter {} in the position {}'.format(letter, index))
+        index += + 1
+
+def ask_guess():
+    guess = input('What letter? ')
+    guess = guess.strip().upper()
+    return guess
 
 def initiate_letters_right(word):
     return ["_" for letter in word]
